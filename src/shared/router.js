7e669d5ea627b3a4ee5bd790e3273c968b895265
wonseok-do <wsdo970@gmail.com>
2022-08-22 01:04:26 +0900

    메인페이지

--- src/shared/router.js
+++ src/shared/router.js
@@ -1,13 +1,14 @@
-import {Route, Routes} from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Example from "../pages/Example";
-
+import Home from "../pages/Home";
 
 const Router = () => {
   return (
-      <Routes>
-        <Route path="/" element={<Example/>}/>
-      </Routes>
-  )
-}
+    <Routes>
+      <Route path="/" element={<Example />} />
+      <Route path="/home" element={<Home />} />
+    </Routes>
+  );
+};
 
-export default Router
+export default Router;
