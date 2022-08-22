52f4968d20792ae4a3064c8c5e408462698009ef
wonseok-do <wsdo970@gmail.com>
2022-08-22 22:25:17 +0900

    pagination추가

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,15 @@
 import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import axios from "axios";
+import Pagination from "../Pagination";
+import { BsFillEmojiHeartEyesFill } from "react-icons/bs";
 
 const Home = () => {
   const [postList, setPostList] = useState([]);
+  const [limit, setLimit] = useState(4);
+  const [page, setPage] = useState(1);
+  const offset = (page - 1) * limit;
+
   useEffect(() => {
     const getPosts = async () => {
       const res = await axios.get("http://localhost:5000/post");
@@ -11,26 +17,52 @@ const Home = () => {
     };
     getPosts();
   }, []);
-  console.log(postList);
+
   return (
-    <StContainer>
-      {postList.map((post) => {
-        return (
-          <StCard key={post.id}>
-            <StImg>
-              <img src={post.img} />
-            </StImg>
-            <StComment>
-              <span>{post.title}</span>
-              <span>{post.nickname}</span>
-              <span>{post.price}원</span>
-              <span>{post.location}</span>
-              <span style={{ textAlign: "right" }}>♡{post.likeCount}</span>
-            </StComment>
-          </StCard>
-        );
-      })}
-    </StContainer>
+    <>
+      <label>
+        페이지 당 표시할 게시물 수:&nbsp;
+        <select
+          type="number"
+          value={limit}
+          onChange={({ target: { value } }) => setLimit(Number(value))}
+        >
+          <option value="4">4</option>
+          <option value="8">8</option>
+          <option value="12">12</option>
+        </select>
+      </label>
+      <StContainer>
+        {postList.slice(offset, offset + limit).map((post) => {
+          return (
+            <StCard key={post.id}>
+              <StImg>
+                <img src={post.img} alt="post" />
+              </StImg>
+              <StComment>
+                <span>{post.title}</span>
+                <span>{post.nickname}</span>
+                <span>{post.price}원</span>
+                <span>{post.location}</span>
+                {/* <span style={{ textAlign: "right" }}>♡{post.likeCount}</span> */}
+                <div style={{ textAlign: "right" }}>
+                  <BsFillEmojiHeartEyesFill />
+                  {post.likeCount}
+                </div>
+              </StComment>
+            </StCard>
+          );
+        })}
+      </StContainer>
+      <footer>
+        <Pagination
+          total={postList.length}
+          limit={limit}
+          page={page}
+          setPage={setPage}
+        />
+      </footer>
+    </>
   );
 };
 
