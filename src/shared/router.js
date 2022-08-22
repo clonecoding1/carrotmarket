de44dd5de7b712382cb66c0645627d740a3b9a7a
SomFist <89966037+somfist@users.noreply.github.com>
2022-08-22 22:22:06 +0900

    디테일 완성 (#11)
    Co-authored-by: qkrruddjs317@naver.com <솜주먹>

--- src/shared/router.js
+++ src/shared/router.js
@@ -1,17 +1,20 @@
-import {Navigate, Route, Routes} from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import Example from "../pages/Example";
 import Login from "../pages/Login";
 import Write from "../pages/Write";
 import Mypage from "../pages/Mypage";
+import Detail from "../pages/Detail";
 
 const Router = () => {
+  // 나중에 로그인 유무에 따른 alert를 여기서 전부 처리하기
   return (
     <Routes>
-      <Route path="/" element={<Example/>}/>
-      <Route path="/login" element={<Login/>}/>
-      <Route path="/write" element={<Write/>}/>
-      <Route path="/mypage" element={<Mypage/>}/>
-      <Route path="/*" element={<Navigate to="/"/>}/>
+      <Route path="/" element={<Example />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/write" element={<Write />} />
+      <Route path="/mypage" element={<Mypage />} />
+      <Route path="/detail/:postId" element={<Detail />} />
+      <Route path="/*" element={<Navigate to="/" />} />
     </Routes>
   );
 };
