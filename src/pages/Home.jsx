4a1f2cdeb5a81de7cc9f727a80bb82cd778dddd0
somfist <qkrruddjs317@naver.com>
2022-08-23 20:34:58 +0900

    2차 에러수정+라우터 조정

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,8 +3,11 @@ import styled from "styled-components";
 import axios from "axios";
 import Pagination from "../Pagination";
 import { BsFillEmojiHeartEyesFill } from "react-icons/bs";
+import { AiOutlinePlus } from "react-icons/ai";
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
+  const nav = useNavigate();
   const [postList, setPostList] = useState([]);
   const [limit, setLimit] = useState(4);
   const [page, setPage] = useState(1);
@@ -19,14 +22,10 @@ const Home = () => {
   }, []);
 
   return (
-    <>
+    <div style={{ position: "relative", height: "100%" }}>
       <label>
         페이지 당 표시할 게시물 수:&nbsp;
-        <select
-          type="number"
-          value={limit}
-          onChange={({ target: { value } }) => setLimit(Number(value))}
-        >
+        <select type="number" value={limit} onChange={({ target: { value } }) => setLimit(Number(value))}>
           <option value="4">4</option>
           <option value="8">8</option>
           <option value="12">12</option>
@@ -42,9 +41,7 @@ const Home = () => {
               <StComment>
                 <span className="title">{post.title}</span>
                 <span className="nickname">{post.nickname}</span>
-                <span className="price">
-                  {Number(post.price).toLocaleString()}원
-                </span>
+                <span className="price">{Number(post.price).toLocaleString()}원</span>
                 <span className="location">{post.location}</span>
                 {/* <span style={{ textAlign: "right" }}>♡{post.likeCount}</span> */}
                 <div style={{ textAlign: "right" }}>
@@ -57,14 +54,17 @@ const Home = () => {
         })}
       </StContainer>
       <footer>
-        <Pagination
-          total={postList.length}
-          limit={limit}
-          page={page}
-          setPage={setPage}
-        />
+        <Pagination total={postList.length} limit={limit} page={page} setPage={setPage} />
       </footer>
-    </>
+      <AddBtn
+        className="fcc"
+        onClick={() => {
+          nav("/write");
+        }}
+      >
+        <AiOutlinePlus />
+      </AddBtn>
+    </div>
   );
 };
 
@@ -113,5 +113,26 @@ const StComment = styled.div`
   & .location {
     font-size: medium;
     color: #3a3939;
+  }
+`;
+
+const AddBtn = styled.button`
+  position: absolute;
+  bottom: 5rem;
+  right: 5rem;
+  border-radius: 50%;
+  border: 1px solid rgb(255, 138, 61);
+  background-color: rgb(255, 138, 61);
+
+  width: 8rem;
+  height: 8rem;
+
+  font-size: 5rem;
+  font-weight: bold;
+  color: white;
+
+  &:hover {
+    background-color: white;
+    color: rgb(255, 138, 61);
   }
 `;
