455522060817e4cd6fc11e68cedfbf6ff2caed3f
somfist <qkrruddjs317@naver.com>
2022-08-23 22:34:08 +0900

    현재까지 기능 작동확인 완료 => 이상한 뷰 상의필요

--- src/components/login/Signup.jsx
+++ src/components/login/Signup.jsx
@@ -38,12 +38,11 @@ const Signup = ({ goSignin }) => {
       swalert("error", "닉네임 중복검사를 다시 확인해주세요");
       return;
     }
-    swalert("success", "굿");
-    // postSignup({ ...data, profile }).then((res) => {
-    //   swalert("success", "회원가입이 완료되었습니다.");
-    //   goSignin();
-    //   e.target.reset();
-    // });
+    postSignup({ ...data, profile }).then((res) => {
+      swalert("success", "회원가입이 완료되었습니다.");
+      goSignin();
+      e.target.reset();
+    });
   };
 
   //이메일 수정할때 다시 false로 돌림
@@ -60,7 +59,6 @@ const Signup = ({ goSignin }) => {
     }
   }, [nickname.current]);
 
-  console.log(dupEmailText, dupNicknameText);
   //이메일 중복검사
   const onDupEmail = () => {
     const regex = /\S+@\S+\.\S+/;
@@ -126,12 +124,8 @@ const Signup = ({ goSignin }) => {
             <button onClick={onDupEmail} type={"button"}>
               중복체크
             </button>
-            {errors.email && errors.email.type === "required" && (
-              <p className={"warning"}>이메일은 필수 입력사항입니다</p>
-            )}
-            {errors.email && errors.email.type === "pattern" && (
-              <p className={"warning"}>이메일 형식에 맞지 않습니다</p>
-            )}
+            {errors.email && errors.email.type === "required" && <p className={"warning"}>이메일은 필수 입력사항입니다</p>}
+            {errors.email && errors.email.type === "pattern" && <p className={"warning"}>이메일 형식에 맞지 않습니다</p>}
           </StInputWrapper>
           <StInputWrapper>
             <input
@@ -157,15 +151,9 @@ const Signup = ({ goSignin }) => {
             <button onClick={onDupNickname} type={"button"}>
               중복체크
             </button>
-            {errors.nickname && errors.nickname.type === "required" && (
-              <p className={"warning"}>닉네임은 필수 입력사항 입니다.</p>
-            )}
-            {errors.nickname && errors.nickname.type === "minLength" && (
-              <p className={"warning"}>{errors.nickname.message}</p>
-            )}
-            {errors.nickname && errors.nickname.type === "maxLength" && (
-              <p className={"warning"}>{errors.nickname.message}</p>
-            )}
+            {errors.nickname && errors.nickname.type === "required" && <p className={"warning"}>닉네임은 필수 입력사항 입니다.</p>}
+            {errors.nickname && errors.nickname.type === "minLength" && <p className={"warning"}>{errors.nickname.message}</p>}
+            {errors.nickname && errors.nickname.type === "maxLength" && <p className={"warning"}>{errors.nickname.message}</p>}
           </StInputWrapper>
           <StInputWrapper>
             <input
@@ -180,22 +168,14 @@ const Signup = ({ goSignin }) => {
                   message: "8자리 이상 비밀번호를 사용하세요",
                 },
                 pattern: {
-                  value:
-                    /^.*(?=^.{8,}$)(?=.*\d)(?=.*[a-zA-Z])(?=.*[!@#$%^&+=]).*$/,
-                  message:
-                    "비밀번호는 문자, 숫자, 특수문자 각 1개씩 포함하며 8글자 이상입니다",
+                  value: /^.*(?=^.{8,}$)(?=.*\d)(?=.*[a-zA-Z])(?=.*[!@#$%^&+=]).*$/,
+                  message: "비밀번호는 문자, 숫자, 특수문자 각 1개씩 포함하며 8글자 이상입니다",
                 },
               })}
             />
-            {errors.password && errors.password.type === "required" && (
-              <p className={"warning"}>비밀번호는 필수 입력사항 입니다</p>
-            )}
-            {errors.password && errors.password.type === "minLength" && (
-              <p className={"warning"}>{errors.password.message}</p>
-            )}
-            {errors.password && errors.password.type === "pattern" && (
-              <p className={"warning"}>{errors.password.message}</p>
-            )}
+            {errors.password && errors.password.type === "required" && <p className={"warning"}>비밀번호는 필수 입력사항 입니다</p>}
+            {errors.password && errors.password.type === "minLength" && <p className={"warning"}>{errors.password.message}</p>}
+            {errors.password && errors.password.type === "pattern" && <p className={"warning"}>{errors.password.message}</p>}
           </StInputWrapper>
           <StInputWrapper>
             <input
@@ -207,16 +187,10 @@ const Signup = ({ goSignin }) => {
                 validate: (value) => value === password.current,
               })}
             />
-            {errors.confirmPassword &&
-              errors.confirmPassword.type === "required" && (
-                <p className={"warning"}>
-                  재확인 비밀번호는 필수 입력사항 입니다.
-                </p>
-              )}
-            {errors.confirmPassword &&
-              errors.confirmPassword.type === "validate" && (
-                <p className={"warning"}>비밀번호가 일치하지 않습니다.</p>
-              )}
+            {errors.confirmPassword && errors.confirmPassword.type === "required" && (
+              <p className={"warning"}>재확인 비밀번호는 필수 입력사항 입니다.</p>
+            )}
+            {errors.confirmPassword && errors.confirmPassword.type === "validate" && <p className={"warning"}>비밀번호가 일치하지 않습니다.</p>}
           </StInputWrapper>
           <StInputWrapper>
             <select {...register("location")}>
