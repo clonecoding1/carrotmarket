b54d729d034d3833eb3bca5d96c47d852c6c1735
qkrruddjs317@naver.com <>
2022-08-24 11:55:56 +0900

    담당 작업 재분배

--- src/utils/swal.js
+++ src/utils/swal.js
@@ -7,3 +7,7 @@ export const successAlert = () => {
 export const errorAlert = () => {
   return Swal.fire({ icon: "error", title: "글쓰기 실패", confirmButtonText: "홈으로 이동" });
 };
+
+export const nonePageAlert = () => {
+  return Swal.fire({ icon: "info", title: "구현중인 기능입니다!", confirmButtonText: "확인" });
+};
